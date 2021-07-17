e233ced2980a4dfe4fafb4bc6127ba45f9e89c03
Andrew <vlietstra123@hotmail.com>
2021-07-17 12:39:25 -0400

    made some changes

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
-    return <form>
+const ExpenseForm = (props) => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+    
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: '',
+    // });
+
+    function titleChangeHandler(event) {
+        setEnteredTitle(event.target.value);
+
+        // One way of capturing the input with state
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // });
+
+        // Another way of capturing the input with state
+        // setUserInput((prevState) => {
+        //     return {...prevState, enteredTitle: event.target.value};
+        // });
+    }
+
+    function amountChangeHandler(event) {
+        setEnteredAmount(event.target.value);
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // });
+    }
+
+    function dateChangeHandler(event) {
+        setEnteredDate(event.target.value);
+    
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value,
+        // });
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault(); // prevent page refresh (prevent default behaviour of the form submit action)
+
+        // wrap up our data into an object
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate),
+        };
+        console.log(expenseData);
+
+        props.onSaveExpenseData(expenseData);
+
+        // To clear the input fields after submitting
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
+
+    return <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text'></input>
+                    <input type='text' value={enteredTitle} onChange={titleChangeHandler}></input>
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01'></input>
+                    <input type='number' min='0.01' step='0.01' value={enteredAmount} onChange={amountChangeHandler}></input>
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2019-01-01' max='2022-12-31'></input>
+                    <input type='date' min='2019-01-01' max='2022-12-31' value={enteredDate} onChange={dateChangeHandler}></input>
                 </div>
             </div>
             <div className='new-expense__actions'>
