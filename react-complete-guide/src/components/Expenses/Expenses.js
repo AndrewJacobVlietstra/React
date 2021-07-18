a9fdb8eb42d46120e4693068c0ce9cf532e2357b
Andrew <vlietstra123@hotmail.com>
2021-07-18 12:14:34 -0400

    Made my expense list output dynamically using the map method

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,42 +1,34 @@
-import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
-import './Expenses.css';
-import Card from '../UI/Card';
-import ExpensesFilter from './ExpensesFilter';
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+  // set up state to track currently selected year
+  const [selectedYear, setSelectedYear] = useState("2020");
 
-  const [selectedYear, setSelectedYear] = useState('2020');
-
+  // use a function to capture the state of the selected year
   const saveSelectValueHandler = (expectedCurrentValue) => {
     setSelectedYear(expectedCurrentValue);
-  }
+  };
 
   return (
-      <Card className='expenses'>
-        <ExpensesFilter defaultYear={selectedYear} onSaveSelectValue={saveSelectValueHandler} />
+    <Card className="expenses">
+      <ExpensesFilter
+        defaultYear={selectedYear}
+        onSaveSelectValue={saveSelectValueHandler}
+      />
+      {props.items.map((expense) => (
         <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
         />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
-      </Card>
+      ))}
+    </Card>
   );
-};
+}
 
 export default Expenses;
