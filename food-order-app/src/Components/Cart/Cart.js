23cc7f52d78d5c2fddab58677e907533ea67c33b
Andrew <vlietstra123@hotmail.com>
2021-07-26 20:45:34 -0400

    Started working on making the cart display items properly

--- food-order-app/src/Components/Cart/Cart.js
+++ food-order-app/src/Components/Cart/Cart.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useContext} from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import CartContext from "../../Store/cart-context";
 
 function Cart(props) {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+      {cartCtx.items.map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
@@ -17,12 +23,12 @@ function Cart(props) {
 
       <div className={classes.total}>
           <span>Total Amount</span>
-          <span>25.62</span>
+          <span>{totalAmount}</span>
       </div>
 
       <div className={classes.actions}>
           <button className={classes['button--alt']} onClick={props.onCloseCart}>Close</button>
-          <button className={classes.button}>Order</button>
+          {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
