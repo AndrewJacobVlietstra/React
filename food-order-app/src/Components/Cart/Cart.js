6adfabc5275addf6f86cd6f2175fed1583f7c93b
Andrew <vlietstra123@hotmail.com>
2021-07-27 10:56:33 -0400

    Made items addible and removeable from within cart

--- food-order-app/src/Components/Cart/Cart.js
+++ food-order-app/src/Components/Cart/Cart.js
@@ -1,7 +1,8 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartContext from "../../Store/cart-context";
+import CartItem from "./CartItem";
 
 function Cart(props) {
   const cartCtx = useContext(CartContext);
@@ -9,10 +10,25 @@ function Cart(props) {
   const totalAmount = `${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1});
+  };
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
   const cartItems = (
-    <ul className={classes['cart-items']}>
+    <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -22,13 +38,15 @@ function Cart(props) {
       {cartItems}
 
       <div className={classes.total}>
-          <span>Total Amount</span>
-          <span>{totalAmount}</span>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
       </div>
 
       <div className={classes.actions}>
-          <button className={classes['button--alt']} onClick={props.onCloseCart}>Close</button>
-          {hasItems && <button className={classes.button}>Order</button>}
+        <button className={classes["button--alt"]} onClick={props.onCloseCart}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
