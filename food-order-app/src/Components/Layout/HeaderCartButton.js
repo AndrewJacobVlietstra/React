09aa917aa83770c96d92cee69f72aa71d96712a5
Andrew <vlietstra123@hotmail.com>
2021-07-27 11:07:34 -0400

    used useEffect to make a bump animation whenever cart items change

--- food-order-app/src/Components/Layout/HeaderCartButton.js
+++ food-order-app/src/Components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartContext from '../../Store/cart-context';
 import './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
@@ -6,15 +6,30 @@ import classes from './HeaderCartButton.module.css';
 
 
 function HeaderCartButton(props) {
-
+    const [bumpBtn, setBumpBtn] = useState(false);
     const cartCtx = useContext(CartContext);
     
     const numberOfCartItems = cartCtx.items.reduce((prevTotal, currentItem) => {
         return prevTotal + currentItem.amount;
     }, 0);
 
+    const btnClasses = `${classes.button} ${bumpBtn ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (cartCtx.items.length === 0) return;
+        setBumpBtn(true);
+
+        const timer = setTimeout(() => {
+            setBumpBtn(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [cartCtx.items]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
