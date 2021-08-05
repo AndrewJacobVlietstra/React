96bf28e984b62a494156c73b5a48681e982989ed
Andrew <vlietstra123@hotmail.com>
2021-08-04 20:56:35 -0400

    Looked at how when a component that manages state has its state changed, that component and all its children and childrens children components will also be re-evaluated

--- optimization-techniques/src/App.js
+++ optimization-techniques/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
 
-import './App.css';
+import "./App.css";
+import DemoOutput from "./components/DemoOutput/DemoOutput";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  console.log('App Rendered!');
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph}/>
+      <Button onClick={() => setShowParagraph(!showParagraph)}>
+        Show Paragraph!
+      </Button>
     </div>
   );
 }
---
+++ optimization-techniques/src/components/DemoOutput/DemoOutput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function DemoOutput({show}) {
+    return (
+        <p>{show ? 'This is Showing' : null}</p>
+    );
+};
+
+export default DemoOutput;
