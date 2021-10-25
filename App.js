8fbeb6197ce5c2de679deebaacc5faca197523cf
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-10-25 10:11:43 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import List from './components/List';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Header />
+      <List />
+    </div>
+  );
+}
+
+export default App;
